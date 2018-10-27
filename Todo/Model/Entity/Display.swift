bc280d80b990fc2c43f204e5dd7d3f583e2c364a
droideveloper <fatih.sen.boun@gmail.com>
2018-10-27 11:48:51 +0300

    bussines logic for store and restore todo implemeted

---
+++ Todo/Model/Entity/Display.swift
@@ -0,0 +1,13 @@
+//
+//  Display.swift
+//  Todo
+//
+//  Created by Fatih Şen on 27.10.2018.
+//  Copyright © 2018 Fatih Şen. All rights reserved.
+//
+
+import Foundation
+
+public enum Display {
+	case all, active, inactive
+}
